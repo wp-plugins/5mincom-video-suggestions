f80a2a70b40efebc930f945e506896d8c3c302b7
adylevy <adylevy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-16 12:49:01 +0000

    ** Version 1.
    git-svn-id: https://plugins.svn.wordpress.org/5mincom-video-suggestions/trunk@451601 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- FiveminVideoSuggest.php
+++ FiveminVideoSuggest.php
@@ -1,43 +1,47 @@
 <?php
 /***************************************************************************
 
-Plugin Name: FiveMin Video Suggest
+Plugin Name: Aol Video Suggest
 Plugin URI: http://pshared.5min.com/wordpress/FiveMinVideoSuggestWPPlugin.zip
 Description: Suggests videos for your post, based on 5min semantic engine.
 Version: 0.2
 Author: 5Min Media
-Author URI: http://www.5min.com
+Author URI: http://www.5minmedia.com
 
 ***************************************************************************/
 
 //Include class
-require('FiveminVideoSuggestCls.php');
+//require('FiveminVideoSuggestCls.php');
+
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 
 function FiveMinVideoSuggest_admin_init() {
-wp_enqueue_script('fivemin-baseconfig',WP_PLUGIN_URL . "/FiveMinVideoSuggestWPPlugin/js/Fivemin.BaseConfig.js");
+    /*wp_enqueue_script('fivemin-baseconfig',WP_PLUGIN_URL . "/FiveMinVideoSuggestWPPlugin/js/Fivemin.BaseConfig.js");
 	wp_enqueue_script('fivemin-lib',WP_PLUGIN_URL . "/FiveMinVideoSuggestWPPlugin/js/Fivemin.Lib.js");
 	
 	wp_enqueue_script('fivemin-jquery',WP_PLUGIN_URL . "/FiveMinVideoSuggestWPPlugin/js/Fivemin.JQuery.js");
 	wp_enqueue_script('fivemin-lightbox',WP_PLUGIN_URL . "/FiveMinVideoSuggestWPPlugin/js/Fivemin.lightbox.js");
 	wp_enqueue_script('fivemin-video-suggest',WP_PLUGIN_URL . "/FiveMinVideoSuggestWPPlugin/js/Fivemin.wp.plugin.js");
-	wp_enqueue_style('fivemin-video-css',  WP_PLUGIN_URL . "/FiveMinVideoSuggestWPPlugin/css/style.css");
+	wp_enqueue_style('fivemin-video-css',  WP_PLUGIN_URL . "/FiveMinVideoSuggestWPPlugin/css/style.css");*/
+	
+	wp_enqueue_script('fivemin-plugin',"http://pshared.5min.com/Scripts/Plugin.js?c=0&v=2");
+	wp_enqueue_style('fivemin-video-css',  "http://pshared.5min.com/Css/Plugin/Base.css");
+	
 }
 
 function FiveMinVideoSuggest_init() {
-	add_meta_box('FiveMinVideoSuggest', 'FiveMin Video Suggest', 'FiveMinVideoSuggestBox', 'post', 'normal', 'high');
+	add_meta_box('FiveMinVideoSuggest', 'Aol Video Suggest', 'FiveMinVideoSuggestBox', 'post', 'side', 'high');
 }
 function FiveMinVideoSuggestBox() {
+	$options = get_option('videoSuggest_options');
+	$sid =  (intval($options['sid']));
+	if ($sid==0)
+		$sid=203;
+	
 	?>
 	<div class='fivemin-videosuggestbox'>
-		<div class='head'>
-			<input type=text class='txt_ipt'/>
-			<input type="button" class="button" onclick="FIVEMIN.VideoSuggestPlugin.getSearchResults()" value="Search" />
-			<input type="button" class="button" onclick="FIVEMIN.VideoSuggestPlugin.getVideoSuggest()" value="Get Video Suggestions" />
-		</div>
-		<div id="FiveMin_VideoSuggestion_Msg">&nbsp;</div>
-		<br/>
-		<div id="FiveMin_VideoSuggestions" >&nbsp;	</div>
-		<div style="clear: both"></div>
+		<div id="fivemin-plugin" data-api="Wordpress" data-params="sid=<?=$sid?>"></div>
 	</div>
 <?php	
 }
@@ -63,8 +67,9 @@ function add_fivemin_to_mce_options( $init ) {
 
 function add_5min_video($content) {
 	$szSearchPattern = '~<img class="fiveminVideoPlayer" [^>]* />~';
-	
-	preg_match_all($szSearchPattern, $content, $media);
+	$pattern = '/<img .*class="fiveminVideoPlayer" [^>]*>/';
+ 	preg_match_all($pattern , $content, $media);
+
 	for($i=0; $i<count($media[0]); $i++)
 	{
 		$img = ($media[0][$i]);
@@ -74,13 +79,20 @@ function add_5min_video($content) {
 		*/
 		if (count($params)!=0){
 			$paramsAttribute= $params[1][0];
-			// todo split by comma and implement parameters ....
-
-			$videoId = $paramsAttribute;
-
-			$vCode = "<div style='text-align:center'><object width='480' height='401' id='FiveminPlayer' classid='clsid:d27cdb6e-ae6d-11cf-96b8-444553540000'><param name='allowfullscreen' value='true'/><param name='allowScriptAccess' value='always'/><param name='movie' value='http://embed.5min.com/".$videoId."/&sid=203'/><embed name='FiveminPlayer' src='http://embed.5min.com/".$videoId."/&sid=203' type='application/x-shockwave-flash' width='480' height='401' allowfullscreen='true' allowScriptAccess='always'></embed></object></div>";
 			
-			$content = str_replace($img, $vCode, $content);
+			$splitted= explode('|||', $paramsAttribute);
+            $paramsArray=array();
+			for($j=0;$j<=count($splitted);$j++){
+				$keyValuePair=explode('=',$splitted[$j]);
+				$paramsArray[$keyValuePair[0]]=$keyValuePair[1];
+			}
+			$videoId = $paramsArray['videoId'];
+			
+			$passedParams=implode("&",$splitted);
+			//$passedParams=substr($passedParams,1);
+			$playerSeed = '<div style="overflow:hidden;"><script type="text/javascript" src="http://pshared.5min.com/Scripts/PlayerSeed.js?'.$passedParams.'"></script></div>';
+			
+			$content = str_replace($img, $playerSeed, $content);
 			
 		}
 	}
@@ -93,15 +105,15 @@ add_filter('the_content','add_5min_video');
 // validate fivemin attributes in img element inside tinymce
 add_filter('tiny_mce_before_init', 'add_fivemin_to_mce_options');
 //Register an AJAX hook for the get videos function
-add_action('wp_ajax_FiveMin_getVideos', 'FiveMin_getVideos');
-add_action('wp_ajax_FiveMin_getSearchResults', 'FiveMin_getSearchResults');
+//add_action('wp_ajax_FiveMin_getVideos', 'FiveMin_getVideos');
+//add_action('wp_ajax_FiveMin_getSearchResults', 'FiveMin_getSearchResults');
 //Initialization to add the box to the post page
 add_action('admin_menu', 'FiveMinVideoSuggest_init');
 //Initialization to add css and js
 add_action('admin_init', 'FiveMinVideoSuggest_admin_init');
 
 
-
+/*
 function FiveMin_getSearchResults() {
 	header('Content-type: application/json');
 	$term = stripslashes($_POST['searchTerm']);
@@ -122,5 +134,67 @@ function FiveMin_getVideos() {
 
 	die($jsonResponse);	
 }
+*/
+
+/* settings */
+
+// add the admin options page
+add_action('admin_menu', 'plugin_admin_add_page');
+
+// Add settings link on plugin page
+function videoSuggest_settings_link($links) { 
+  $settings_link = '<a href="options-general.php?page=videoSuggest">Settings</a>'; 
+  array_unshift($links, $settings_link); 
+  return $links; 
+}
+ 
+$plugin = plugin_basename(__FILE__); 
+add_filter("plugin_action_links_$plugin", 'videoSuggest_settings_link' );
+
+function plugin_admin_add_page() {
+add_options_page('Video Suggest', 'Video Suggest', 'manage_options', 'videoSuggest', 'videoSuggest_options_page');
+}
+
+// display the admin options page
+function videoSuggest_options_page() {
+?>
+<div>
+<h2>Video suggest plugin</h2>
+Options relating to the Plugin.
+<form action="options.php" method="post">
+<?php settings_fields('videoSuggest_options'); ?>
+<?php do_settings_sections('videoSuggest'); ?>
+
+<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+</form></div>
+
+<?php
+}
+
+// add the admin settings and such
+add_action('admin_init', 'plugin_admin_init');
+function plugin_admin_init(){
+	register_setting( 'videoSuggest_options', 'videoSuggest_options' );
+	add_settings_section('videoSuggest_main', 'Main Settings', 'videoSuggest_section_text', 'videoSuggest');
+	add_settings_field('videoSuggest_text_string', 'Syndicator Id', 'videoSuggest_setting_string', 'videoSuggest', 'videoSuggest_main');
+	//add_settings_field('videoSuggest_text_string2', 'Something else', 'videoSuggest_setting_string2', 'videoSuggest', 'videoSuggest_main');
+}
+
+function videoSuggest_section_text() {
+	//echo '<p>Main description of this section here.</p>';
+}
+
+function videoSuggest_setting_string() {
+$options = get_option('videoSuggest_options');
+echo "<input id='videoSuggest_text_string' name='videoSuggest_options[sid]' size='40' type='text' value='{$options['sid']}' />";
+echo "</br>";
+} 
+/*
+function videoSuggest_setting_string2() {
+$options = get_option('videoSuggest_options');
+echo "<input id='videoSuggest_text_string2' name='videoSuggest_options[something]' size='40' type='text' value='{$options['something']}' />";
+echo "</br>";
+} */
 
 ?>
+
--- readme.txt
+++ readme.txt
@@ -1,30 +1,28 @@
 === Plugin Name ===
 
 Contributors: adylevy
-Plugin Name: FiveMin Video Suggest
-Plugin URI: http://shared.5min.com/wordpress/FiveMinVideoSuggestWPPlugin.zip
-Version: 0.2
-Author: 5Min Media
-Author URI: http://www.5min.com
+Plugin Name: Aol Video Suggest
+Plugin URI: http://www.5minmedia.com
+Version: 1.0
+Author: Aol Video
+Author URI: http://www.5minmedia.com
 Tags: video, semantic
 
 Requires at least: 2.9
 Tested up to: 3.1.1
 Stable tag: trunk
 
-Suggests videos for your post, based on 5min semantic engine.
+Suggests videos for your post, based on Aol Video semantic engine.
 
 == Description ==
 
-You just finished writing a great post and you would like to add a video to it ?
-Hit the video suggest button and instantly grab a video from 5min.com great video library!
+Adds a video suggestions box to your wordpress admin area.
 
 == Installation ==
 
 
 1. Upload the folders contents to the `/wp-content/plugins/` directory
-2. Make sure you have write permissions on your wordpress upload folder
-3. Activate the plugin through the 'Plugins' menu in WordPress
+2. Activate the plugin through the 'Plugins' menu in WordPress
 
 
 == Frequently Asked Questions ==
@@ -32,24 +30,17 @@ Hit the video suggest button and instantly grab a video from 5min.com great vide
 
 == Screenshots ==
 
-1. The video suggestions box
-2. Video lightbox
+1. Customization Area
+2. The video suggestions box
 
 == Changelog ==
 
-= 0.2.1 =
+= 0.0.1 =
+First release.
 
-1. Add to post button for each video.
+= 1.0 ==
+Better and improved release.
 
-= 0.2 =
-
-1. Code refactoring
-2. Nicer lightbox 
-3. Added search functionality 
-
-= 0.1 =
-
-1.First release.
 
 
 
