3082230acd5539e96530f1b918d1112a5f734055
adylevy <adylevy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-05 13:04:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/5mincom-video-suggestions/trunk@514640 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- FiveminVideoSuggest.php
+++ FiveminVideoSuggest.php
@@ -4,187 +4,114 @@
 Plugin Name: Aol Video Suggest
 Plugin URI: http://www.5minmedia.com
 Description: Suggests videos for your post, based on Aol Video semantic engine.
-Version: 1.0
+Version: 1.1
 Author: 5Min Media
 Author URI: http://www.5minmedia.com
 
 ***************************************************************************/
 
+class FiveMinVideoSuggest {
 
-
-function FiveMinVideoSuggest_admin_init() {
-   
-	
-	wp_enqueue_script('fivemin-plugin',"http://pshared.5min.com/Scripts/Plugin.js?c=0&v=2");
-	wp_enqueue_style('fivemin-video-css',  "http://pshared.5min.com/Css/Plugin/Base.css");
-	
-}
-
-function FiveMinVideoSuggest_init() {
-	add_meta_box('FiveMinVideoSuggest', 'Aol Video Suggest', 'FiveMinVideoSuggestBox', 'post', 'side', 'high');
-}
-function FiveMinVideoSuggestBox() {
-	$options = get_option('videoSuggest_options');
-	$sid =  (intval($options['sid']));
-	if ($sid==0)
-		$sid=203;
-	
-	?>
-	<div class='fivemin-videosuggestbox'>
-		<div id="fivemin-plugin" data-api="Wordpress" data-params="sid=<?=$sid?>"></div>
-	</div>
-<?php	
-}
-
-
-
-// this function will authorize the data-product / data-params attributes we want to chuck into the img element.
-function add_fivemin_to_mce_options( $init ) {
-    // Command separated string of extended elements
-    $ext = 'img[*]';
-
-    // Add to extended_valid_elements if it alreay exists
-    if ( isset( $init['extended_valid_elements'] ) ) {
-        $init['extended_valid_elements'] .= ',' . $ext;
-    } else {
-        $init['extended_valid_elements'] = $ext;
-    }
-
-    // Super important: return $init!
-    return $init;
-}
-
-
-function add_5min_video($content) {
-	$szSearchPattern = '~<img class="fiveminVideoPlayer" [^>]* />~';
-	$pattern = '/<img .*class="fiveminVideoPlayer" [^>]*>/';
- 	preg_match_all($pattern , $content, $media);
-
-	for($i=0; $i<count($media[0]); $i++)
-	{
-		$img = ($media[0][$i]);
-		preg_match_all('/data-params="(.+?)"/',$img,$params);
-		/*
-		Array ( [0] => Array ( [0] => data-params="517061814" ) [1] => Array ( [0] => 517061814 ) ) Array ( [0] => Array ( [0] => data-params="500048780" ) [1] => Array ( [0] => 500048780 ) ) 
-		*/
-		if (count($params)!=0){
-			$paramsAttribute= $params[1][0];
-			
-			$splitted= explode('|||', $paramsAttribute);
-            $paramsArray=array();
-			for($j=0;$j<=count($splitted);$j++){
-				$keyValuePair=explode('=',$splitted[$j]);
-				$paramsArray[$keyValuePair[0]]=$keyValuePair[1];
-			}
-			$videoId = $paramsArray['videoId'];
-			
-			$passedParams=implode("&",$splitted);
-			//$passedParams=substr($passedParams,1);
-			$playerSeed = '<div style="overflow:hidden;"><script type="text/javascript" src="http://pshared.5min.com/Scripts/PlayerSeed.js?'.$passedParams.'"></script></div>';
-			
-			$content = str_replace($img, $playerSeed, $content);
-			
-		}
+	public function __construct() {
+		add_filter( 'the_content', array( $this, 'the_content' ) );
+		add_filter( 'tiny_mce_before_init', array( $this, 'tiny_mce_before_init' ) );
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+		add_action( 'admin_print_styles-post.php', array( $this, 'admin_print_styles' ) );
+		add_action( 'admin_print_styles-post-new.php', array( $this, 'admin_print_styles' ) );
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
 	}
-	return $content;
+
+	function admin_print_styles() {
+		wp_enqueue_script('fivemin-plugin',"http://pshared.5min.com/Scripts/Plugin.js?c=0&v=2");
+		wp_enqueue_style('fivemin-video-css',  "http://pshared.5min.com/Css/Plugin/Base.css");
+	}
+
+	function add_meta_boxes() {
+		add_meta_box('FiveMinVideoSuggest', 'Aol Video Suggest', array( $this, 'meta_box_callback' ), 'post', 'side', 'high');
+	}
+
+	function meta_box_callback() {
+		$options = get_option('videoSuggest_options');
+		$sid = ( isset( $options['sid'] ) && 0 != $options['sid'] ) ? intval( $options['sid'] ) : 203;
+		?>
+		<div class='fivemin-videosuggestbox'>
+			<div id="fivemin-plugin" data-api="Wordpress" data-params="sid=<?php echo $sid; ?>"></div>
+		</div>
+		<?php
+	}
+
+	// Makes tinymce allow any attribute in the img element so we can add the data-product / data-params attributes we want.
+	function tiny_mce_before_init( $init ) {
+		$init['extended_valid_elements'] = isset( $init['extended_valid_elements'] ) ? $init['extended_valid_elements'] . ',img[*]' : 'img[*]';
+		return $init;
+	}
+
+
+	// filter the_content to convert img's to videos (thats where the magic is done)
+	function the_content($content) {
+		
+		$pattern = '/<img .*class="fiveminVideoPlayer" [^>]*>/';
+		preg_match_all($pattern , $content, $media);
+
+		for($i=0; $i<count($media[0]); $i++)
+		{
+			$img = ($media[0][$i]);
+			preg_match_all('/data-params="(.+?)"/',$img,$params);
+			/*
+			Array ( [0] => Array ( [0] => data-params="517061814" ) [1] => Array ( [0] => 517061814 ) ) Array ( [0] => Array ( [0] => data-params="500048780" ) [1] => Array ( [0] => 500048780 ) )
+			*/
+			if (count($params)!=0){
+				$paramsAttribute= $params[1][0];
+
+				$splitted= explode('|||', $paramsAttribute);
+				$paramsArray=array();
+				for($j=0;$j<=count($splitted);$j++){
+					$keyValuePair=explode('=',$splitted[$j]);
+					$paramsArray[$keyValuePair[0]]=$keyValuePair[1];
+				}
+
+				$passedParams=implode("&",$splitted);
+				
+				$playerSeed = '<div style="overflow:hidden;"><script type="text/javascript" src="http://pshared.5min.com/Scripts/PlayerSeed.js?'.$passedParams.'"></script></div>';
+
+				$content = str_replace($img, $playerSeed, $content);
+
+			}
+		}
+		return $content;
+	}
+
+
+
+	// Add settings link on plugin page
+	function plugin_action_links($links) {
+		$settings_link = '<a href="options-media.php#videoSuggest">Settings</a>';
+		array_unshift($links, $settings_link);
+		return $links;
+	}
+
+	function admin_init(){
+		register_setting( 'media', 'videoSuggest_options', array( $this, 'sanitize_options' ) );
+		add_settings_section( 'videoSuggest_main', '5min Settings', '__return_false', 'media' );
+		add_settings_field( 'videoSuggest_text_string', 'Syndicator Id', array( $this, 'settings_field_callback' ), 'media', 'videoSuggest_main' );
+
+		add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), array( $this, 'plugin_action_links' ) );
+	}
+
+	function sanitize_options( $input ) {
+		$new_input['sid'] = isset( $input['sid'] ) ? intval( $input['sid'] ) : 0;
+		return $new_input;
+	}
+
+	function settings_field_callback() {
+		$options = get_option('videoSuggest_options');
+		?>
+		<a name="videoSuggest"></a>
+		<input id="videoSuggest_text_string" name="videoSuggest_options[sid]" size="40" type="text" value="<?php if ( isset( $options['sid'] ) ) echo intval( $options['sid'] ); ?>" />
+		</br>
+		<?php
+	}
+
 }
 
-// add pre post render hook so we can convert img's to videos (thats where the magic is done)
-add_filter('the_content','add_5min_video');
-
-// validate fivemin attributes in img element inside tinymce
-add_filter('tiny_mce_before_init', 'add_fivemin_to_mce_options');
-//Register an AJAX hook for the get videos function
-//add_action('wp_ajax_FiveMin_getVideos', 'FiveMin_getVideos');
-//add_action('wp_ajax_FiveMin_getSearchResults', 'FiveMin_getSearchResults');
-//Initialization to add the box to the post page
-add_action('admin_menu', 'FiveMinVideoSuggest_init');
-//Initialization to add css and js
-add_action('admin_init', 'FiveMinVideoSuggest_admin_init');
-
-
-/*
-function FiveMin_getSearchResults() {
-	header('Content-type: application/json');
-	$term = stripslashes($_POST['searchTerm']);
-	
-	$fiveMinVideoSuggest = new FiveminVideoSuggestCls();
-	$jsonResponse = $fiveMinVideoSuggest->getSearchResults($term);	
-
-	die($jsonResponse);	
-}
-
-function FiveMin_getVideos() {
-	header('Content-type: application/json');
-	$content = stripslashes($_POST['text']);
-	$title = stripslashes($_POST['title']);
-	
-	$fiveMinVideoSuggest = new FiveminVideoSuggestCls();
-	$jsonResponse = $fiveMinVideoSuggest->getVideos($content,$title);	
-
-	die($jsonResponse);	
-}
-*/
-
-/* settings */
-
-// add the admin options page
-add_action('admin_menu', 'plugin_admin_add_page');
-
-// Add settings link on plugin page
-function videoSuggest_settings_link($links) { 
-  $settings_link = '<a href="options-general.php?page=videoSuggest">Settings</a>'; 
-  array_unshift($links, $settings_link); 
-  return $links; 
-}
- 
-$plugin = plugin_basename(__FILE__); 
-add_filter("plugin_action_links_$plugin", 'videoSuggest_settings_link' );
-
-function plugin_admin_add_page() {
-add_options_page('Video Suggest', 'Video Suggest', 'manage_options', 'videoSuggest', 'videoSuggest_options_page');
-}
-
-// display the admin options page
-function videoSuggest_options_page() {
-?>
-<div>
-<h2>Video suggest plugin</h2>
-Options relating to the Plugin.
-<form action="options.php" method="post">
-<?php settings_fields('videoSuggest_options'); ?>
-<?php do_settings_sections('videoSuggest'); ?>
-
-<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
-</form></div>
-
-<?php
-}
-
-// add the admin settings and such
-add_action('admin_init', 'plugin_admin_init');
-function plugin_admin_init(){
-	register_setting( 'videoSuggest_options', 'videoSuggest_options' );
-	add_settings_section('videoSuggest_main', 'Main Settings', 'videoSuggest_section_text', 'videoSuggest');
-	add_settings_field('videoSuggest_text_string', 'Syndicator Id', 'videoSuggest_setting_string', 'videoSuggest', 'videoSuggest_main');
-	//add_settings_field('videoSuggest_text_string2', 'Something else', 'videoSuggest_setting_string2', 'videoSuggest', 'videoSuggest_main');
-}
-
-function videoSuggest_section_text() {
-	//echo '<p>Main description of this section here.</p>';
-}
-
-function videoSuggest_setting_string() {
-$options = get_option('videoSuggest_options');
-echo "<input id='videoSuggest_text_string' name='videoSuggest_options[sid]' size='40' type='text' value='{$options['sid']}' />";
-echo "</br>";
-} 
-/*
-function videoSuggest_setting_string2() {
-$options = get_option('videoSuggest_options');
-echo "<input id='videoSuggest_text_string2' name='videoSuggest_options[something]' size='40' type='text' value='{$options['something']}' />";
-echo "</br>";
-} */
-
-?>
-
+$five_min_video_suggest = new FiveMinVideoSuggest;
